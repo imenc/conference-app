6a5672bc1a484001ebf9d018aa3e303c02c120fa
imenc <ahmadpahang@uii.ac.id>
2022-06-27 22:07:43 +0700

    Add bootstrap

--- app/fe/pages/_app.js
+++ app/fe/pages/_app.js
@@ -1,7 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css'; // Add this line
 import '../styles/globals.css'
+import { useEffect } from "react";              // Add this line
+
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    useEffect(() => {
+        import("bootstrap/dist/js/bootstrap");  // Add this line
+    }, []);
+    return <Component {...pageProps} />
 }
 
 export default MyApp
